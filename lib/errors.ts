f91fc8659ee145a0c9a5d4c38d24280baa03cb7c
freehour <ka1@posteo.de>
2026-04-23 06:11:59 +0000

    remove file not supported error

--- lib/errors.ts
+++ lib/errors.ts
@@ -68,49 +68,6 @@ export class FileNotFoundError extends TracedError {
 }
 
 
-export interface FileNotSupportedErrorOptions extends ErrorOptions {
-
-    /**
-     * The name of the file that caused the error.
-     */
-    fileName?: string;
-
-    /**
-     * The type of the file that is not supported.
-     */
-    fileType?: string;
-}
-
-/**
- * An error that indicates that a file is not supported, e.g. due to its MIME type or extension.
- */
-export class FileNotSupportedError extends Error {
-
-    /**
-     * The name of the file that caused the error.
-     */
-    readonly fileName?: string;
-
-    /**
-     * The type of the file that is not supported.
-     */
-    readonly fileType?: string;
-
-    constructor(
-        message: string,
-        {
-            fileName,
-            fileType,
-            ...options
-        }: FileNotSupportedErrorOptions = {},
-    ) {
-        super(message, options);
-        this.fileName = fileName;
-        this.fileType = fileType;
-    }
-}
-
-
 export interface ParseErrorOptions extends ErrorOptions {
 
     /**
